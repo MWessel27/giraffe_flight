a42dcdc3c87ae913549eec99d3cde6bd0eefd14a
MWessel27 <mwessel27@gmail.com>
2018-06-09 20:24:50 -0400

    Checklist functionality

--- thebeautyapp/thebeautyapp/homeViewController.swift
+++ thebeautyapp/thebeautyapp/homeViewController.swift
@@ -11,14 +11,38 @@ import UIKit
 class homeViewController: UIViewController {
     
     @IBOutlet weak var mTableView: UITableView!
+    
+    var products = [String]()
+    
+    let lightOrange:UIColor = UIColor(red: 0.996, green: 0.467, blue: 0.224, alpha: 1)
+    let medOrange:UIColor = UIColor(red: 0.973, green: 0.388, blue: 0.173, alpha: 1)
+    let darkOrange:UIColor = UIColor(red: 0.796, green: 0.263, blue: 0.106, alpha: 1)
+    
+    let green:UIColor = UIColor(red: 0.251, green: 0.831, blue: 0.494, alpha: 1)
+    
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        formatter.locale = Locale(identifier: "en_US")
+        return formatter
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        products = ["Lotion","Moisturizer","Lipstick"]
+        mTableView.tableFooterView = UIView(frame: CGRect.zero)
         
         let rightBarButton = UIBarButtonItem(title: "Products", style: UIBarButtonItemStyle.plain, target: self, action: #selector(goToProdList))
         self.navigationItem.rightBarButtonItem = rightBarButton
         
         let leftBarButton = UIBarButtonItem(title: "Stats", style: UIBarButtonItemStyle.plain, target: self, action: #selector(goToStatsCal))
         self.navigationItem.leftBarButtonItem = leftBarButton
+        
+        mTableView.backgroundColor = darkOrange
+        mTableView.separatorStyle = UITableViewCellSeparatorStyle.none
     }
     
     @objc func goToProdList() {
@@ -27,6 +51,8 @@ class homeViewController: UIViewController {
         let nextViewController = storyBoard.instantiateViewController(withIdentifier: "productViewController")
         self.navigationController?.pushViewController(nextViewController, animated: true)
     }
+    
+    
     
     @objc func goToStatsCal() {
         let storyBoard : UIStoryboard = self.storyboard!
@@ -40,6 +66,42 @@ class homeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
 
+extension homeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "ProductCell")
+        let productName = products[indexPath.row]
+        cell.textLabel?.text = productName
+        cell.backgroundColor = medOrange
+        cell.textLabel?.textColor = UIColor.white
+        cell.detailTextLabel?.textColor = UIColor.gray
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let date = Date()
+        
+        
+        let mySelectedCell:UITableViewCell = tableView.cellForRow(at: indexPath)!
+        mySelectedCell.detailTextLabel?.textColor = UIColor.white
+        mySelectedCell.detailTextLabel?.text = dateFormatter.string(for: date)
+        mySelectedCell.accessoryType = UITableViewCellAccessoryType.checkmark
+        mySelectedCell.tintColor = UIColor.white
+        mySelectedCell.detailTextLabel?.backgroundColor = green
+        mySelectedCell.backgroundColor = green
+    }
+    
 }
 
